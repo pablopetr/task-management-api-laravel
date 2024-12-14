147e319c32868b24c5264ba8e0f5f53475133343
Pablo Eliezer <pabloeliezer@hotmail.com>
2024-12-14 18:53:07 -0300

    Refactor /me route response to use UserResource

--- app/Http/Controllers/User/ProfileController.php
+++ app/Http/Controllers/User/ProfileController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
 
 class ProfileController extends Controller
 {
     public function __invoke()
     {
-        return response()->json(['data' => auth()->user()]);
+        return response()->json(UserResource::make(auth()->user()));
     }
 }
