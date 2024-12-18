251761d5a2e7461457d296b6ae937b012ee0c674
Pablo Eliezer <pabloeliezer@hotmail.com>
2024-12-18 20:26:50 -0300

    Fix UserResource error

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -15,10 +15,10 @@ class UserResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
-            'email_verified_at' => $this->email_verified_at->format('Y-m-d H:i:s'),
+            'email_verified_at' => $this->email_verified_at?->format('Y-m-d H:i:s'),
             'role' => $this->role,
             'created_at' => $this->created_at->format('Y-m-d H:i:s'),
-            'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
+            'updated_at' => $this->updated_at?->format('Y-m-d H:i:s'),
         ];
     }
 }
